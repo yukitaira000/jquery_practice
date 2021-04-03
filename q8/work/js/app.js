a3369292044f1046b38352af4cb3221763e64aad
yukitaira000@gmail.com <yukitaira000@gmail.com>
2021-04-03 10:41:09 +0900

    課題提出

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -8,44 +8,70 @@
 //   displayResult(result)
 // }).fail(function (err) {
   //   displayError(err)
-  // });
-var pageCount = 1;
-$(function(){
-  // 検索ボタンをクリックした時に
-  $('.search-btn').on('click', function(){
-    // 検索ワードに入力されたワードをsearchWordに代入
-    var searchWord = $('#search-input').val();
-    // .messageクラスがあった場合、削除する(同じメッセージの表示が重複しないようにするため。)
-    $('.message').empty();
-    // 検索ワードに何も入力されていない時
-    if($('#search-input').val("")){
-      // listsクラスの前にdiv class="message"を追加しメッセージを表示する
-      $('.lists').before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>')}
-    // 検索ワードに入力された言葉が同じだった場合、追加の検索結果を表示、異なる検索ワードだった場合、新しいページにし表示しなおす。
-    searchWord !== searchWord ? (pageCount = 1, $('.lists').empty()): pageCount = +1;
+// })
 
+
+// 関数宣言
+$(function(){
+  // ページの初期値を1に指定
+  var pageCount = 1;
+  // 検索ボタンをクリック時に
+  $('.search-btn').on('click', function(){
+    // searchWordに検索ワードに入力された値を代入
+    var searchWord = $('#search-input').val();
+    // 検索ワードが同一のものが入力された場合、ページのカウントをそのまま増やし、表示を追加する。異なる検索ワードの場合はページを初期値に戻す
+    searchWord === $('#search.input').val() ? pageCount = +1 : (pageCount = 1,$('.lists').empty());
+    // ajaxのデータ取得先
     const settings = {
     url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
     method: "GET",
     }
-    function result(){
-      if(0 < searchWord){$('.list-item').prepend('<li class="link-item"><div class="list-inner"><p>タイトル：' + result.title + '</p><p>作者：' + result['dc:creator'] + '</p><p>出版社：' + result['dc:publisher'] + '</p><a href "' + result.link['@id'] + '" target="blank">書籍情報</a></div></li>')}else{
-        $('lists').empty();
-        $('lists').before('<div class ="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>')
+    // .done時の処理
+    function displayResult(searchWord){
+      // messageクラスを削除
+      $('.message').remove();
+      // 検索ワードの結果が存在していた場合
+      if (searchWord[0].items != null){
+        // 検索結果内の繰り返し処理を指定
+        // 関数booksの処理を記述
+        $.each(searchWord[0].items, function(searchresult, books){
+          // 変数statusにajaxで取得した情報である、タイトルや作者名、出版社の情報代入。
+          var status = '<li class = "list-item"><div class="list-inner"><p>タイトル：' + ((books.title ? books.title : 'タイトル不明') + '</p><p>作者：') + ((books['dc:creator'] ? books['dc:creator'] : '作者不明') + '</p><p>出版社：' + ((books['dc:publisher'] ? books['dc:publisher'] : '出版社不明') + '</p><a href= "') + ((books.link['@id']) + 'target = "_blank">書籍情報</a></div></li>'));
+          // 取得したあと.listsクラスの子要素の先頭に検索結果を追加していく。
+          $('.lists').prepend(status);
+        })
+        // 検索結果がなかった場合
+      }else {
+        // .messageクラスを削除
+        $('.message').remove();
+        // .listsクラスの子要素を削除
+        $('.lists').empty();
+        // 検索結果がなかったとメッセージを表示
+        $('.lists').before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>');
       }
     }
+    // データが取得できなかった時の処理
+    function displayError(errmessage){
+      // .messageクラスを削除
+      $('.message').remove();
+      // 検索結果が入力なかったためエラーのメッセージを表示
+      $('.lists').before('<div class="message">検索キーワードが有効ではありません。<br>1文字以上で検索して下さい。</div>');
+    }
+    // settingsにしてした情報を取得
     $.ajax(settings)
-      .done(function (response) {
+    // データ取得成功時の処理
+    .done(function (response){
+      // resultにajaxで取得した検索結果の情報を代入
       const result = response['@graph'];
+      // 上記に書いた.done時の処理の関数を呼び出す。
+      // resultに代入された値を取得
       displayResult(result);
-      })
     })
-      .fail(function (err) {
+    // データ取得できなかった際の処理
+    .fail(function(err){
+      // 上記displayError関数を呼び出す。
       displayError(err);
-      if(searchWord === 0){
-        $('.lists').before('<div class ="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>'
-      )};
-    });
+    })
   })
   // リセットボタンをクリックした時
   $('.reset-btn').on('click', function(){
@@ -56,19 +82,4 @@ $(function(){
     // 検索ワードの内容を空欄にする
     $('#search-input').val("");
   })
-  // resultに呼び出す関数を作る
-  // 関数には分岐を作ること
-  // .fail以降は起こりうるエラーに対して作る
-
-
-  
-  // 質問です。
-  // 本の情報を取得する際にコールバック関数を使用すると思うのですが、ここが理解ができていないと思うので、APIからの情報を取得できていないのかと思います。
-  // コールバック関数に関しての質問です。
-  // displayResult(result)を実行し、function(response)に呼ばれた関数を受け取るという認識で間違い無いでしょうか？
-  // ネットや本などの情報を調べてみましたが、それでもうまく情報が取得できないということは何か間違っているのでは無いかと思い、質問させていただきました。
-
-  // 他の項目としては実装時にポイントに合うようにしていますが、間違いや修正が必要な箇所があれば教えていただきたいと思います。
-
-  
-  // よろしくお願い致します。
+})
