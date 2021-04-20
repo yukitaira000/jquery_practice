2d99f24292e4fdde9fc16366448c227b8ed436e4
yukitaira000@gmail.com <yukitaira000@gmail.com>
2021-04-21 01:01:48 +0900

    Q1〜Q7は修正しました。Q8について質問したいため提出

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -2,17 +2,28 @@ $(function(){
   // セレクトボタンの値が変更された時
   $('.select-box').on('change', function(){
     // .select-boxの値を取得しcategoryに代入
-    var category = $(this).val();
+    const category = $(this).val();
     // .food-list liに登録された値を取得
-    var foodList = $('.food-list li');
-    // 全てを選択している時、カテゴリーを全て表示する
-    'all' === category ? foodList.show():
-    // 全てを選択していない場合、以下の処理をする。
-    $.each(foodList, function(selectCategory, list){
-      // 選択されたカテゴリーのcategory-typeの値を取得し、listNameに代入する。
-      var listName = $(list).data('category-type');
-      // .select-boxの値と上記で取得したカテゴリーの値が同じものを表示する。異なるものは表示しない。
-      category === listName ? $(list).show(): $(list).hide()
-    })
+    const foodList = $('.food-list li');
+    // 全てを選択している時、
+    if('all' === category){
+      // カテゴリーを全て表示する
+      foodList.show();
+      // 全てを選択していない時
+    }else{
+      // foodListのkeyに取得、listValにlistの値を取得
+      $.each(foodList, function(key, listVal){
+        // listNameに選択されたカテゴリーのタイプを代入する
+        const listName = $(listVal).data('category-type');
+        // categoryとlistNameが同じ場合
+        if(category === listName){
+          // 選択されたlistを表示
+          $(listVal).show();
+        }else{
+          // 選択されたカテゴリーと異なるリストは表示しない
+          $(listVal).hide();
+        }
+      })
+    }
   })
 })
