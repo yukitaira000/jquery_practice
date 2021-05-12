0df6018b9fb082ec037418bdb9c2d18e91f38773
yukitaira000@gmail.com <yukitaira000@gmail.com>
2021-05-12 23:33:08 +0900

    Q1-6数値型に修正、Q3.11行目のクラスを削除、全体的にvarになっていたのを修正、Q8を全体的に修正

--- q4/work/js/app.js
+++ q4/work/js/app.js
@@ -2,7 +2,7 @@ $(function(){
   // ナビの項目をクリックした時に
   $('.nav li').on('click',function(){
     // ナビのクリックされたindex番号をthisに追加
-    var index = $('.nav li').index(this);
+    const index = $('.nav li').index(this);
     // .description liにis-hiddenを追加する
     // 追加することで下記の操作で削除されたクラスを追加し非表示にする。
     $('.description li').addClass('is-hidden');
